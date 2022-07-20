4b5e6ca9d5cadbcc8609d1d07e0fde158bd5870d
Sandy Tseng <is.sandytseng@gmail.com>
2022-07-20 22:34:40 +0800

    show station name

--- app/Http/Controllers/WaterQualityController.php
+++ app/Http/Controllers/WaterQualityController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Station;
 use App\WaterQuality;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
@@ -120,7 +121,7 @@ class WaterQualityController extends Controller
         $target = $request->get('target', 'PH');
 
         $waterQualityQuery = WaterQuality::query()
-            ->select(['water.id', 'date', 'record_id', 'station.locality_chinese', "water.{$target}"])
+            ->select(['water.id', 'date', 'record_id', 'station.locality_chinese', "water.{$target}", 'station.id as station_id'])
             ->join('station', 'station.id', '=', 'water.id')
             ->whereHas('station', function ($query) use ($locality) {
                 if ($locality) {
@@ -152,10 +153,18 @@ class WaterQualityController extends Controller
             ->orderBy('id')
             ->orderBy('date')->get();
 
+        $stations = Station::select(['id', 'locality', 'locality_chinese'])
+            ->whereIn('id', $waterQuality->unique('station_id')->pluck('station_id'))
+            ->orderBy('station.latitude')
+            ->orderBy('station.longitude')
+            ->get()
+            ->keyBy('id');
+
         $data = $waterQuality->groupBy('id')
-            ->map(function ($records, $stationId) use ($target) {
+            ->map(function ($records, $stationId) use ($target, $stations) {
+                $st = $stations[$stationId];
                 return [
-                    'name' => "測站 $stationId",
+                    'name' => "$st->locality_chinese (#$stationId)",
                     'data' => $records->map(function ($record) use ($target) {
                         return [
                             Carbon::createFromFormat('Y-m-d H:i:s', $record->date)->timestamp*1000,
