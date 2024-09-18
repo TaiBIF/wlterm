e3683a58219b3a207af561b21309fbc43d0ef5f4
Sandy Tseng <is.sandytseng@gmail.com>
2024-09-19 01:10:59 +0800

    Adjust column width

--- resources/js/pages/AlgaeDebris.vue
+++ resources/js/pages/AlgaeDebris.vue
@@ -37,12 +37,12 @@
                 },
                 columns: [
                     { type: 'text', title: '測站', width: '50', name: 'station_id', searchable: true },
-                    { type: 'text', title: '測站站名', width: '100', name: 'locality_chinese', searchable: true },
+                    { type: 'text', title: '測站站名', width: '120', name: 'locality_chinese', searchable: true },
                     { type: 'text', title: '調查日期', width: '160', name: 'date', searchable: true },
                     { type: 'text', title: '調查者', width: '100', name: 'collector_chinese', searchable: true },
-                    { type: 'text', title: '調查項目', width: '160', name: 'item_chinese', searchable: true },
+                    { type: 'text', title: '調查項目', width: '300', name: 'item_chinese', searchable: true },
                     { type: 'text', title: '調查值', width: '100', name: 'biomass'},
-                    { type: 'text', title: '單位', width: '100', name: 'unit'},
+                    { type: 'text', title: '單位', width: '140', name: 'unit'},
                 ],
                 total: 0,
             }
