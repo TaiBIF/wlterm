9067e5aa3c58b3f06b7bd8e891747343f8544edb
Sandy Tseng <is.sandytseng@gmail.com>
2024-09-21 21:39:14 +0800

    Revert "Login button"
    This reverts commit 6e884f3847bcc74d6e6d4ac5834cead6bb027720.

--- resources/js/components/navbar.vue
+++ resources/js/components/navbar.vue
@@ -1,18 +1,11 @@
 <template>
     <header class="blog-header py-3 shadow-md">
         <div class="flex justify-center">
-            <div class="flex flex-1 justify-center">
-                <div class="flex items-center">
-                    <img src="/images/logo.jpg" class="logo">
-                    <a class="blog-header-logo text-dark text-lg" href="#">
-                        武陵地區生態監測及生態模式建立
-                        <small>雪霸國家公園委託辦理計畫</small>
-                    </a>
-                </div>
-            </div>
-            <div class="flex w-[50px] items-center text-lg">
-                <a href='https://wlterm-intra.biodiv.tw' >
-                    登入
+            <div class="title-container">
+                <img src="/images/logo.jpg" class="logo">
+                <a class="blog-header-logo text-dark text-lg" href="#">
+                    武陵地區生態監測及生態模式建立
+                    <small>雪霸國家公園委託辦理計畫</small>
                 </a>
             </div>
         </div>
@@ -43,5 +36,3 @@
         }
     }
 </style>
-<script lang="ts">
-</script>
